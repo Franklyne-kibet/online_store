67d0529811c65ed663df0be28a521f5c1d1a7477
Franklyne-kibet <rootkibet@gmail.com>
2023-05-18 21:54:42 +0300

    utils

---
+++ datagenerator/utils/config.py
@@ -0,0 +1,12 @@
+import os
+
+from datagenerator.utils.db import DBConnection
+
+def get_database_creds() -> DBConnection:
+    return DBConnection(
+        user=os.getenv('DB_USER',''),
+        password=os.getenv('DB_PASSWORD',''),
+        db=os.getenv('DB', ''),
+        host=os.getenv('DB_HOST',''),
+        port=int(os.getenv('DB_PORT', 5432)),
+    )
